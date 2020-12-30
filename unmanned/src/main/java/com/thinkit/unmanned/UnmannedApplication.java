8d2a5c02181cb8f9ff403035d7239a6554f9a76d
map1211 <twins887605@hansung.ac.kr>
2020-12-30 13:37:48 +0900

    2020.12.30 LSB (master) 최초 커밋

---
+++ unmanned/src/main/java/com/thinkit/unmanned/UnmannedApplication.java
@@ -0,0 +1,13 @@
+package com.thinkit.unmanned;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnmannedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnmannedApplication.class, args);
+	}
+
+}
---
+++ unmanned/src/test/java/com/thinkit/unmanned/UnmannedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkit.unmanned;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnmannedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
